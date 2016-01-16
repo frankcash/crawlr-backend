ee189df402be7d02c5122cde790d488e70bc468a
Frank Cash <cfcash@mtu.edu>
2016-01-16 01:22:17 -0500

    Adds app.js and updates package.json

---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+ * * Module dependencies
+ * */
+
+var config = require('./config/development');
+var request = require('request');
+var express = require('express');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+// var nodemailer = require('nodemailer');
+// var async = require('async');
+// var crypto = require('crypto');
+
+
+var app = express(); // sets up the server
+
+
+mongoose.connect(config.db);;
+
+app.use(express.logger('dev'))
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// setting up passport authentication
+require('./passport')(passport)
+app.use(session({ secret: 'invitable invitables'}));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/routes')(app, passport);
+
+app.use(express.static(__dirname + '/public'))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+console.log("invitable is now running");
+app.listen(3000)
