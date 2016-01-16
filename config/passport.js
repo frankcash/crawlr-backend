ba5fbd9a72c1f56c8178e7bffdd21c048d351874
Jacob Jenkins <jacob@jacobejenkins.com>
2016-01-16 02:39:51 -0500

    Add passport.js

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+let passport = require('passport');
+let User = require('../models/User');
+let LocalStrategy = require('passport-local').Strategy;
+
+// Serialize session
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// Deserialize sessions
+passport.deserializeUser(function(id, done) {
+    User.findOne({
+        _id: id
+    }, '-salt -password', function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}, function (username, password, done) {
+    User.findOne({
+        username: username.toLowerCase()
+    }, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user || !user.authenticate(password)) {
+            return done(null, false, {
+                message: 'Invalid username or password'
+            });
+        }
+
+        return done(null, user);
+    });
+}));
