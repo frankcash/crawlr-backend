31b5f78ab4f47d6b633066517f185dfac74ddeae
Jacob Jenkins <jacob@jacobejenkins.com>
2016-01-16 03:09:08 -0500

    Route skeleton

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,18 @@
 module.exports = function(app, passport) {
+    app.route('/crawls')
+        .get()
+        .put()
+        .post();
 
+    app.route('/crawls/:crawlID')
+        .get();
+
+    app.route('/bars/:barID')
+        .get();
+
+    app.route('/signup')
+        .post();
+    
+    app.route('/signin')
+        .post();
 };
