9c15bcbd17f1079540bebde6bd0ae652efe15aaa
Jacob Jenkins <jacob@jacobejenkins.com>
2016-01-16 04:23:22 -0500

    Here's a rout

--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,40 @@
-var Bar = require('../models/bar');
-var Crawl = require('../models/crawl');
+'use strict';
+let Bar = require('../models/bar');
+let Crawl = require('../models/crawl');
 
 module.exports = function(app, passport) {
     app.route('/crawls')
-        .get((req, res)=>{
-           const getAll = req.query.all;
+        .get((req, res) => {
+            const getAll = req.query.all;
         })
-        .put((req, res)=>{
+        .put((req, res) => {
 
         })
-        .post((req, res)=>{
-
+        .post((req, res) => {
+            let crawl = new Crawl(req.body);
+            crawl.save((err, crawl) => {
+                if (err) res.sendStatus(500);
+                else res.status(200).json(crawl);
+            });
         });
 
     app.route('/crawls/:crawlID')
-        .get((req, res)=>{
-          const crawlID = req.params.crawlID;
+        .get((req, res) => {
+            const crawlID = req.params.crawlID;
         });
 
     app.route('/bars/:barID')
-        .get((req, res)=>{
+        .get((req, res) => {
 
         });
 
     app.route('/signup')
-        .post((req, res)=>{
+        .post((req, res) => {
 
         });
 
     app.route('/signin')
-        .post((req, res)=>{
+        .post((req, res) => {
 
         });
 };
