c1d054667eaf9aec2113361b472a3d5829043bab
Jacob Jenkins <jacob@jacobejenkins.com>
2016-01-16 03:37:36 -0500

    Add dev config

--- config/development.js
+++ config/development.js
@@ -1,5 +1,13 @@
 var config = {};
 
-config.db = "";
+config.db = 'mongodb://localhost/crawlr-dev';
+config.sessionCookie = {
+    maxAge: 8640000,
+    httpOnly: true,
+    secure: false
+};
+config.sessionSecret = 'TRASHDUMPYARD';
+config.sessionKey = 'sessionId';
+config.sessionCollection = 'sessions';
 
 module.exports = config;
